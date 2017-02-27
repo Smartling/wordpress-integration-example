2fcafbfa53c9b6f61ae4706f7897b92d06452008
Pavlo Ratushnyi <pratushnyi@smartling.com>
2017-02-27 21:17:11 +0200

    description update

--- src/Declarations/FieldFilters.php
+++ src/Declarations/FieldFilters.php
@@ -56,34 +56,31 @@ class FieldFilters
 
             "fields" => [
                 [
+                    "pattern"       => "<field name or regex>", // required
 
-                    "filed_pattern" => "<field name or regex>", // required
-                    "behavior"      => [ // required
-                                         // What to do with field value:
-                                         // * "copy" - simple copy value from source to target
-                                         // * "skip" - don't copy value. Target will have empty value unless it will be populated somehow else
-                                         // * "localize" - send value for translation or transform it. Use this action for references also
-                                         "action"        => "copy|skip|localize", // required
-                                         // Preprocess value before localize it
-                                         // * "none" - no extra preprocessing for value
-                                         // * "delimited" - value should be converted to array. An example "1, 4, 10"
-                                         // * "array" - value is serialized php array
-                                         // * "json" - value is json
-                                         "serialization" => "none|delimited|array|json", // implemented 'none' and 'coma-separated'
-                                         // optional, default none
-                                         // How to handle value
-                                         // * "raw" - use value as is
-                                         // * "reference" - value is reference to another entity (post\tag\attachment\etc)
-                                         // * "url" - value is url to attachment (file\image)
-                                         "value"         => "raw|reference", // required implemented ONLY 'reference'
-                                         // Type of reference\url
-                                         // post - any post-based type
-                                         // media - image or attachment
-                                         // taxonomy:<taxonomy_name> - e.g.: taxonomy:category
-                                         // virtual:<typename> - tells that type if fully virtual
-                                         "type"          => "post( any post-based content-type)|media (file or image or audio or vide) |wp-type",
-                                         // if action != skip ==> required
-                    ],
+                    // What to do with field value:
+                    // * "copy" - simple copy value from source to target
+                    // * "skip" - don't copy value. Target will have empty value unless it will be populated somehow else
+                    // * "localize" - send value for translation or transform it. Use this action for references also
+                    "action"        => "copy|skip|localize", // required
+
+                    // Preprocess value before localize it
+                    // * "none"             - means that fiels contains single id
+                    // * "array-value"      - means that value is an array that is already dererialized
+                    // * "coma-delimited"   - means that malue contains list of ids separated by coma
+                    // * "<custom>"         - means that a custom searializer may be applied
+                    "serialization" => "none|array-value|coma-delimited|<custom>", // required if action=localize
+
+                    // How to handle value
+                    // * "raw" - use value as is
+                    // * "reference" - value is reference to another entity (post\tag\attachment\etc)
+                    "value"         => "raw|reference", // required implemented ONLY 'reference'
+
+                    // Type of reference\url
+                    // post - any post-based type
+                    // media - image or attachment
+                    // <custom_type> - e.g.: taxonomy:category
+                    "type"          => "post|media|<custom_type>", // required if action=localize
                 ],
             ],
         ];
